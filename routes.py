557ced69f17b1b9dbfd1d44618601ab1b4bd9cc2
Sthefany Clarismundo Lima <76106711+stehfany@users.noreply.github.com>
2024-04-16 11:05:05 -0300

    Add files via upload

---
+++ routes.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from src.controler.controler import *
+from src.controler.erros import *
+from flask import Blueprint, render_template, request, redirect, jsonify
+from src.db import *
+
+routes = {
+        "home": "/",
+        "cadastro": "/cadastro",
+        "perguntas":"/perguntas",
+        "resultados":"/resultados",
+        "login":"/login",
+        "reset":"/reset"      
+    }
+
+
+
